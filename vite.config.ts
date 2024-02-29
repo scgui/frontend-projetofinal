3fa14d2cfd301f8d489f9db640e5025f2c880c37
Guilherme Costa <guics37.go@gmail.com>
2024-02-29 14:43:37 -0300

    aa

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/frontend/build/",
+  base: "/frontend-projetofinal/build/",
   build: {
     outDir: 'build',
     assetsDir: 'assets'
